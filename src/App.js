215fabad4290d48c7e099fe300e202d0a6097926
MaltaWebDev <MaltaWebDev@gmail.com>
2023-12-03 13:18:17 +0100

    Add charger item and Item component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Charger", quantity: 1, packed: true },
 ];
 
 function Logo() {
   return <h1>🌴 Far Away 👜</h1>;
 }
+
 function Form() {
   return (
     <div className="add-form">
@@ -13,8 +15,29 @@ function Form() {
     </div>
   );
 }
+
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span>
+        {item.quantity}&nbsp;
+        {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
@@ -28,7 +51,7 @@ function Stats() {
 function App() {
   return (
     <div className="app">
-      <Logo />;
+      <Logo />
       <Form />
       <PackingList />
       <Stats />
