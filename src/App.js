10eed5a40f0afe21bf42872aef20b0fba17d3807
MaltaWebDev <MaltaWebDev@gmail.com>
2023-12-03 11:23:06 +0100

    Add initialItems to App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 function Logo() {
   return <h1>🌴 Far Away 👜</h1>;
 }
@@ -14,7 +19,7 @@ function PackingList() {
 
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>👜You have X items on your list, and you already packed X (X%)</em>
     </footer>
   );
