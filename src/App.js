0d2a29b1f6196274036510584a3e2f4635a42ab0
MaltaWebDev <MaltaWebDev@gmail.com>
2023-12-04 19:07:48 +0100

    Update item ID in initialItems array

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 2, description: "Charger", quantity: 1, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
 ];
 
 function Logo() {
@@ -11,6 +11,7 @@ function Logo() {
 function Form() {
   function handleSubmit(e) {
     e.preventDefault();
+    console.log(e);
   }
 
   return (
