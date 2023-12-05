f8706fa775f50ff572d3694f7770f4049fe604b0
MaltaWebDev <MaltaWebDev@gmail.com>
2023-12-05 21:32:25 +0100

    Fix form submission bug

--- src/App.js
+++ src/App.js
@@ -19,6 +19,8 @@ function Form() {
     if (!description) return;
     const newItem = { description, quantity, packed: false, id: Date.now() };
     console.log(newItem);
+    setDescription("");
+    setQuantity(1);
   }
 
   const numbersArray = [];
