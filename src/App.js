1f0c313d7d0f4be6928c61a4d8c0a3f20caed2c6
MaltaWebDev <MaltaWebDev@gmail.com>
2023-12-07 13:22:59 +0100

    Remove initialItems array and commented code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import { useState } from "react";
 
-// const initialItems = [
-//   { id: 1, description: "Passports", quantity: 2, packed: false },
-//   { id: 2, description: "Socks", quantity: 12, packed: false },
-//   { id: 3, description: "Charger", quantity: 1, packed: true },
-// ];
-
 function Logo() {
   return <h1>🌴 Far Away 👜</h1>;
 }
